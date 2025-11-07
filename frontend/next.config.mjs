af73615c63db94e296f025534c184eb0e7afe7de
Fernando Leon Franco <fernanlee2131@gmail.com>
2025-11-06 20:55:22 -0600

    feat: actualizar configuración de backend y frontend
    - Actualizar destinos en las reescrituras de next.config.mjs para apuntar al backend.
    - Modificar el archivo _events.json con nueva duración de sesión.
    - Eliminar archivo de bloqueo en el frontend.
    - Actualizar archivos binarios de la base de datos en el backend.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,8 +3,8 @@
 const nextConfig = {
   async rewrites() {
     return [
-      { source: '/sim/:path*', destination: 'http://localhost:8000/sim/:path*' },
-      { source: '/health', destination: 'http://localhost:8000/health' },
+      { source: '/sim/:path*', destination: 'http://backend:8000/sim/:path*' },
+      { source: '/health',     destination: 'http://backend:8000/health' },
     ];
   },
   allowedDevOrigins: ['http://localhost:3000', 'http://192.168.100.5:3000'],
