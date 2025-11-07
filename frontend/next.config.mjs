3a8957d8ac37b4a6ff3457349977dca0dd1692fa
Fernando Leon Franco <fernanlee2131@gmail.com>
2025-11-06 21:02:37 -0600

    feat: actualizar configuraciones de red y rutas del backend
    - Se agregó la configuración de redes para el servicio backend en docker-compose.yml.
    - Se actualizó la URL del backend en next.config.mjs para usar el nombre del contenedor.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,15 @@
-// next.config.mjs
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
     return [
-      { source: '/sim/:path*', destination: 'http://backend:8000/sim/:path*' },
-      { source: '/health',     destination: 'http://backend:8000/health' },
+      { source: '/sim/:path*', destination: 'http://sapal-backend:8000/sim/:path*' },
+      { source: '/health',     destination: 'http://sapal-backend:8000/health' },
     ];
   },
-  allowedDevOrigins: ['http://localhost:3000', 'http://192.168.100.5:3000'],
+  allowedDevOrigins: [
+    'http://localhost:3000',
+    'http://192.168.100.5:3000',
+    'https://dashboard-sapal-vaqcb1-4e8607-89-116-212-100.traefik.me',
+    'http://dashboard-sapal-vaqcb1-4e8607-89-116-212-100.traefik.me'
+  ],
 };
 export default nextConfig;
